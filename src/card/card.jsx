e510125fce50df20a2317245c12aa706ce47eb9d
rene2412 <hrene2412@gmail.com>
2025-09-10 22:14:04 -0700

    working on card design

--- src/card/card.jsx
+++ src/card/card.jsx
@@ -1,7 +1,29 @@
 import React from "react"
 import "./card.css"
-export default function Card() {
-        return (
-            <div className="cards"></div>
+
+const num_values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+const face_values = ['K', 'Q', 'J'];
+const suits = [
+    { symbol: '♣️', color: "black" },
+    { symbol: '♠️', color: "black" },
+    { symbol: '♥️', color: "red"   },
+    { symbol: '♦️', color: "red"   }
+]
+
+export default function Card({value, suit}) {
+    const deck = []; 
+        for (let k = 0; k < suits.length; k++) {
+            for (let i = 0; i < num_values.length; i++) {
+                const suit = suits[k];
+                const value = num_values[i];
+                deck.push(
+                   <div key={value + suit.symbol} className={`card ${suit.color}`}> 
+                        <div className="top-left">{value}{suit.symbol}</div>
+                    </div>
+                );
+           }
+    }
+        return (   
+                <div className="deck">{deck} </div>            
         );
 }
